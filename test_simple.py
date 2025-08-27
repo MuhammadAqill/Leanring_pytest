c09b1fc9d1bd624ecaa82045d33f8fdcdfba9cde
MuhammadAqill <maqil5697@gmail.com>
2025-08-27 21:33:46 +0800

    try write for failing passed code

--- test_simple.py
+++ test_simple.py
@@ -6,5 +6,22 @@ def test_simple_add():
 
 @pytest.mark.skip
 def test_failing():
-    assert 'a' == 'b'
+    assert 'a' == 'b' # kita mark untuk skip
+                      # output akan keluar s
+                      # s -> skip
 
+@pytest.mark.xfail
+def test_x_failing():
+    assert 'a' == 'b' # apakah 'a' == 'b' ? tidak
+                      # output akan keluar x
+                      # x -> fail
+                      # bermaksud salah
+
+@pytest.mark.xfail
+def test_X_failing():
+    assert 'a' == 'a' # apakah 'a' == 'a' ? benar
+                      # output akan keluar X
+                      # x -> true
+                      # bermaksud benar
+                      # tetapi kita mark ia untuk salah
+                      # dengan ini kita tahu ia benar tetapi kita set ia salah dengan @pytest.mark.xfail
